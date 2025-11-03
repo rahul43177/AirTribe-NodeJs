b3cad63d9d2f4d4ef950ed83df0a7576e194196d
Rahul Mishra <rahulmishra4317@gmail.com>
2025-11-03 15:45:48 +0530

    assignment for the path and query params

--- S3-RestAndExpress-2/1-MiniCourseAPI/controller/coursesController.js
+++ S3-RestAndExpress-2/1-MiniCourseAPI/controller/coursesController.js
@@ -1,10 +1,63 @@
-const getCourses = async (req , res ) => {
+const coursesDB = [
+    {
+        id : 1 , 
+        courseName : "Backend Course" ,
+        coursecourseDescription : "Master the backend" 
+    } , 
+    {
+        id : 2 , 
+        courseName : "Frontend Course" , 
+        courseDescription : "Master the frontend"
+    } ,
+    {
+        id : 3 , 
+        courseName : "Database course" , 
+        courseDescription : "Master the database"
+    } ,
+    {
+        id : 4 , 
+        courseName : "System Design Course" , 
+        courseDescription : "Master the System Design"
+    } ,
+    {
+        id : 5 , 
+        courseName : "Data Structures and Algorithms Course" , 
+        courseDescription : "Master the DSA"
+    } , 
+    {
+        id : 6 , 
+        courseName : "Mock Interview Course" , 
+        courseDescription : "Master the technical interviews"
+    } , 
+    {
+        id : 7 , 
+        courseName : "High Level Design Course" , 
+        courseDescription : "Master the HLD interviews" 
+    } , 
+    {
+        id : 8  , 
+        courseName : "Low Level Design Course" , 
+        courseDescription : "Low Level Design Course" , 
+        
+    }
+]
+
+const getAllCourses = async (req , res ) => {
     try {
         
+        
+        console.log(req.query)
+
+        res.json({
+            status : true , 
+            jsonData : dataInParams
+        })
     } catch(err) {
         res.json({
             status : false , 
-            err 
+            erorr : err.message
         })
     }
 }
+
+module.exports = {getAllCourses}
--- S3-RestAndExpress-2/1-MiniCourseAPI/routes/courseRouter.js
+++ S3-RestAndExpress-2/1-MiniCourseAPI/routes/courseRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
-
+const {getCourses} = require("../controller/coursesController")
+router.get("/queryCourse" , getCourses)
 
 
 module.exports = router; 
