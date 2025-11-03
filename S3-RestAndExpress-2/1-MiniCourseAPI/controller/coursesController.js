b139aa851321c7079f84337895fec3035583c66f
Rahul Mishra <rahulmishra4317@gmail.com>
2025-11-04 00:25:36 +0530

    Assignment : Completed 2 APIs

--- S3-RestAndExpress-2/1-MiniCourseAPI/controller/coursesController.js
+++ S3-RestAndExpress-2/1-MiniCourseAPI/controller/coursesController.js
@@ -2,56 +2,83 @@ const coursesDB = [
     {
         id : 1 , 
         courseName : "Backend Course" ,
-        coursecourseDescription : "Master the backend" 
+        coursecourseDescription : "Master the backend" , 
+        category : "backend"
     } , 
     {
         id : 2 , 
         courseName : "Frontend Course" , 
-        courseDescription : "Master the frontend"
+        courseDescription : "Master the frontend" , 
+        category : "frontend"
     } ,
     {
         id : 3 , 
         courseName : "Database course" , 
-        courseDescription : "Master the database"
+        courseDescription : "Master the database" , 
+        category : "backend"
     } ,
     {
         id : 4 , 
         courseName : "System Design Course" , 
-        courseDescription : "Master the System Design"
+        courseDescription : "Master the System Design", 
+        category : "Engineering"
     } ,
     {
         id : 5 , 
         courseName : "Data Structures and Algorithms Course" , 
-        courseDescription : "Master the DSA"
+        courseDescription : "Master the DSA" , 
+        category : "Fundamentals"
     } , 
     {
         id : 6 , 
         courseName : "Mock Interview Course" , 
-        courseDescription : "Master the technical interviews"
+        courseDescription : "Master the technical interviews" , 
+        category : "Fundamentals"
     } , 
     {
         id : 7 , 
         courseName : "High Level Design Course" , 
-        courseDescription : "Master the HLD interviews" 
+        courseDescription : "Master the HLD interviews"  , 
+        category : "Backend"
     } , 
     {
         id : 8  , 
         courseName : "Low Level Design Course" , 
         courseDescription : "Low Level Design Course" , 
-        
+        category : "Backend" 
+    } , 
+    {
+        id : 9 , 
+        courseName : "CSS Mastery" , 
+        courseDescription : "Master the CSS for best UI" , 
+        category : "Frontend"
+    } , 
+    {
+        id : 10 , 
+        courseName : "Database Management System" , 
+        courseDescription : "Master the DBMS as a DBA" , 
+        category : "Backend"
     }
 ]
 
-const getAllCourses = async (req , res ) => {
+const getCoursesByCategory = async (req , res ) => {
     try {
-        
-        
-        console.log(req.query)
+        const {category} = req.query;
+        console.log("Category" , category);
+        if(!category) {
+            console.log("Inside the not found - category")
+            return res.json({
+                status : true , 
+                courseData : coursesDB 
+            })
+        } else {
+            const filteredData = coursesDB.filter(singleCourse => singleCourse.category.toLowerCase() == category.toLowerCase());
+            res.json({
+                status : true  , 
+                courseData : filteredData
+            })
+        }
 
-        res.json({
-            status : true , 
-            jsonData : dataInParams
-        })
     } catch(err) {
         res.json({
             status : false , 
@@ -60,4 +87,25 @@ const getAllCourses = async (req , res ) => {
     }
 }
 
-module.exports = {getAllCourses}
+const getCourseById = async (req , res ) => {
+    const {id} = req.params ;  //path parameter -> single value -> telling which value we need. this is path parameter. 
+    if(!id) return res.json({
+        status : false ,  
+        message : "Id is not provided"
+    }) 
+
+    const filteredData = coursesDB.filter((singleCourse) => {
+        return singleCourse.id == id; 
+    }); 
+    return res.json({
+        status : true , 
+        courseData : filteredData
+    })
+}
+
+
+
+module.exports = {
+    getCoursesByCategory , 
+    getCourseById
+}
--- S3-RestAndExpress-2/1-MiniCourseAPI/routes/courseRouter.js
+++ S3-RestAndExpress-2/1-MiniCourseAPI/routes/courseRouter.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {getCourses} = require("../controller/coursesController")
-router.get("/queryCourse" , getCourses)
+const {getCoursesByCategory , getCourseById} = require("../controller/coursesController")
 
+//courses based on category 
+router.get("/category-course" ,getCoursesByCategory);
+
+//courses by id 
+router.get("/course-id/:id", getCourseById);
 
 module.exports = router; 
