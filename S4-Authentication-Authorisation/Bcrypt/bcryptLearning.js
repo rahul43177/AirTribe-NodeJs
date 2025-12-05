25d717144ffd5cc68c7e2931a6503d56dddccdbd
Rahul Mishra <rahulmishra4317@gmail.com>
2025-12-05 15:29:54 +0530

    notes sync

--- S4-Authentication-Authorisation/Bcrypt/bcryptLearning.js
+++ S4-Authentication-Authorisation/Bcrypt/bcryptLearning.js
@@ -13,5 +13,21 @@ let securedPassword;
 hashedPassword.then((data) => {
   securedPassword = data; 
 }).then(() => {
-  console.log(`password : ${password} => hashedPassword : ${securedPassword}`)
+  // console.log(`password : ${password} => hashedPassword : ${securedPassword}`)
 })
+
+
+//$2b$10$n8n20JKPUUaADj70pJJMwu6ChfZkZK54DMN8MvY8aqID6UtGrN1Xy
+// 
+
+async function comparePassword(plainPassword) {
+  //fetch stored password from data 
+  //query = select password from users where name = 'Rahul'
+  //const hashedStoredPassword = execute(query);
+  const hashedPassword = "$2b$10$n8n20JKPUUaADj70pJJMwu6ChfZkZK54DMN8MvY8aqID6UtGrN1Xy"
+  const match = await bcrypt.compare(plainPassword, hashedPassword); 
+  console.log(match);
+}
+
+
+comparePassword(password);
