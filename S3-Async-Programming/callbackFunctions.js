c55dc09cfa0d6e13a614c6a2f7e7d4ddb6850a4b
Rahul Mishra <rahulmishra4317@gmail.com>
2025-12-15 00:16:13 +0530

    syncing notes

--- S3-Async-Programming/callbackFunctions.js
+++ S3-Async-Programming/callbackFunctions.js
@@ -1,34 +1,31 @@
+
 const asyncFunction1 = (cb) => {
     setTimeout(() => {
-        console.log("Async function 1")
+        console.log("Async function 1");
         cb();
     },1000)
-}   
-const asyncFunction2 = (cb) => {
-    setTimeout(() => {
-        console.log("Async function 2")
-        cb();
-    },1000)
-}   
-const asyncFunction3 = (cb) => {
-    setTimeout(() => {
-        console.log("Async function 3")
-        cb();
-    },1000)
-}   
-
-
-
-
-const main = () => {
-    console.log("Start")
-    asyncFunction1(() => {
-        asyncFunction2(() => {
-            asyncFunction3(() => {
-                console.log("All completed");
-            })
-        })
-    }) 
 }
 
-main()
+const asyncFunction2 = (cb) => {
+    setTimeout(() => {
+        console.log("Async function 2");
+        cb();
+    },1000)
+}
+
+const asyncFunction3 = (cb) => {
+    setTimeout(() => {
+        console.log("Async function 3");
+        cb();
+    },1000)
+}
+//developer 
+
+const main = () => {
+    console.log("Start"); 
+    asyncFunction(() => {
+        console.log("End");
+    })
+}
+
+main();
